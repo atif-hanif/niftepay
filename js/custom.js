27e1c7d558720cb5505216c634e0ceaf15628063
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2022-11-07 17:15:15 +0500

    07-11-2022 (2)

--- js/custom.js
+++ js/custom.js
@@ -62,3 +62,18 @@ function fixStepIndicator(n) {
 $(document).ready(function () {
     $("#cnic").mask("99999-9999999-9");
 });
+
+var deadline = new Date("Nov 7, 2022 18:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("timer").innerHTML = "<div class='minutes'>" + minutes + "</div> : <div class='seconds'>" + seconds + '</div>';
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000);
