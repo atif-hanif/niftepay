53dc3b412d676716555c7d740b456cc30dca717a
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2022-11-10 16:33:57 +0500

    10-11-2022 (1)

--- js/custom.js
+++ js/custom.js
@@ -1,63 +1,3 @@
-//your javascript goes here
-var currentTab = 0;
-document.addEventListener("DOMContentLoaded", function(event) {
-
-
-    showTab(currentTab);
-
-});
-
-function showTab(n) {
-    var x = document.getElementsByClassName("tab");
-    x[n].style.display = "block";
-    if (n == 0) {
-        document.getElementById("prevBtn").style.display = "none";
-    } else {
-        document.getElementById("prevBtn").style.display = "inline";
-    }
-    if (n == (x.length - 1)) {
-        document.getElementById("nextBtn").innerHTML = "Pay <i class='fa-solid fa-arrow-right'></i>";
-    } else {
-        document.getElementById("nextBtn").innerHTML = "Next <i class='fa-solid fa-arrow-right'></i>";
-    }
-    fixStepIndicator(n)
-}
-
-function nextPrev(n) {
-    var x = document.getElementsByClassName("tab");
-    if (n == 1 && !validateForm()) return false;
-    x[currentTab].style.display = "none";
-    currentTab = currentTab + n;
-    if (currentTab >= x.length) {
-        // document.getElementById("regForm").submit();
-        // return false;
-        //alert("sdf");
-        document.getElementById("nextprevious").style.display = "none";
-        // document.getElementById("thank-you").style.display = "block";
-    }
-    showTab(currentTab);
-}
-
-function validateForm() {
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName("tab");
-    y = x[currentTab].getElementsByTagName("input");
-    for (i = 0; i < y.length; i++) {
-        if (y[i].value == "") {
-            y[i].className += " invalid";
-            valid = false;
-        }
-    }
-    if (valid) { document.getElementsByClassName("step")[currentTab].className += " finish"; }
-    return valid;
-}
-
-function fixStepIndicator(n) {
-    var i, x = document.getElementsByClassName("step");
-    for (i = 0; i < x.length; i++) { x[i].className = x[i].className.replace(" active", ""); }
-    x[n].className += " active";
-}
-
 $(document).ready(function () {
     $("#cnic").mask("99999-9999999-9");
     $("#account-no").mask("9999999999999999");
@@ -89,11 +29,123 @@ function clickEvent(first,last){
     }
 }
 
-// $(document).ready(function () {
-//     $(document).on('click' , '.nextBtn', function(e) {
-//         if ($('#thank-you').hasClass('d-none')) {
-//             $('#thank-you').removeClass('d-none');
-//             $('#payment').addClass('d-none');
-//         }
-//     });
-// });
+let timerOn = true;
+
+function otptimer(remaining) {
+    var m = Math.floor(remaining / 60);
+    var s = remaining % 60;
+  
+    m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
+    document.getElementById('otp-timer').innerHTML = m + ':' + s;
+    remaining -= 1;
+  
+    if(remaining >= 0 && timerOn) {
+        setTimeout(function() {
+            otptimer(remaining);
+        }, 1000);
+        return;
+    }
+
+    if(!timerOn) {
+    // Do validate stuff here
+        return;
+    }
+}
+
+otptimer(120);
+
+// Smart Wizrd
+
+$(document).ready(function() {
+	$(document).on('click', '.finish', function(e) {
+		if($('#thank-you').hasClass('d-none')) {
+			$('#thank-you').removeClass('d-none');
+			$('#thank-you').addClass('d-block');
+			$('#payment').addClass('d-none');
+		}
+	});
+
+	$(document).on('click', '.home', function(e) {
+		if($('#thank-you').hasClass('d-block')) {
+			$('#thank-you').addClass('d-none');
+			$('#payment').removeClass('d-none');
+		}
+	});
+
+});
+
+// Smart Wizard
+
+$(function() {
+	// Leave step event is used for validating the forms
+	$("#smartwizard").on("leaveStep", function(e, anchorObject, currentStepIdx, nextStepIdx, stepDirection) {
+		// Validate only on forward movement  
+		if(stepDirection == 'forward') {
+			let form = document.getElementById('form-' + (currentStepIdx + 1));
+			if(form) {
+				if(!form.checkValidity()) {
+					form.classList.add('was-validated');
+					$('#smartwizard').smartWizard("setState", [currentStepIdx], 'error');
+					$("#smartwizard").smartWizard('fixHeight');
+					return false;
+				}
+				$('#smartwizard').smartWizard("unsetState", [currentStepIdx], 'error');
+			}
+		}
+	});
+	// Step show event
+	$("#smartwizard").on("showStep", function(e, anchorObject, stepIndex, stepDirection, stepPosition) {
+		$("#prev-btn").removeClass('disabled').prop('disabled', false);
+		$("#next-btn").removeClass('disabled').prop('disabled', false);
+		if(stepPosition === 'first') {
+			$("#prev-btn").addClass('disabled').prop('disabled', true);
+		} else if(stepPosition === 'last') {
+			$("#next-btn").addClass('disabled').prop('disabled', true);
+		} else {
+			$("#prev-btn").removeClass('disabled').prop('disabled', false);
+			$("#next-btn").removeClass('disabled').prop('disabled', false);
+		}
+		// Get step info from Smart Wizard
+		let stepInfo = $('#smartwizard').smartWizard("getStepInfo");
+		$("#sw-current-step").text(stepInfo.currentStep + 1);
+		$("#sw-total-step").text(stepInfo.totalSteps);
+		if(stepPosition == 'last') {
+			$("#btnFinish").prop('disabled', false);
+		} else {
+			$("#btnFinish").prop('disabled', true);
+		}
+	});
+	// Smart Wizard
+	$('#smartwizard').smartWizard({
+		selected: 0,
+		// autoAdjustHeight: false,
+		theme: 'arrows', // basic, arrows, square, round, dots
+		transition: {
+			animation: 'none'
+		},
+		toolbar: {
+			showNextButton: true, // show/hide a Next button
+			showPreviousButton: true, // show/hide a Previous button
+			position: 'bottom', // none/ top/ both bottom
+			extraHtml: `<button class="btn btn-success finish ms-2" id="btnFinish" disabled onclick="onConfirm()">Complete</button>`
+                              //<button class="btn btn-danger" id="btnCancel" onclick="onCancel()">Cancel</button>
+		},
+		anchor: {
+			enableNavigation: true, // Enable/Disable anchor navigation 
+			enableNavigationAlways: false, // Activates all anchors clickable always
+			enableDoneState: true, // Add done state on visited steps
+			markPreviousStepsAsDone: true, // When a step selected by url hash, all previous steps are marked done
+			unDoneOnBackNavigation: true, // While navigate back, done state will be cleared
+			enableDoneStateNavigation: true // Enable/Disable the done state navigation
+		},
+	});
+	$("#state_selector").on("change", function() {
+		$('#smartwizard').smartWizard("setState", [$('#step_to_style').val()], $(this).val(), !$('#is_reset').prop("checked"));
+		return true;
+	});
+	$("#style_selector").on("change", function() {
+		$('#smartwizard').smartWizard("setStyle", [$('#step_to_style').val()], $(this).val(), !$('#is_reset').prop("checked"));
+		return true;
+	});
+});
