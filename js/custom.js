210b8ade43146754fc1c85440a8d9cf7b792c696
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2022-11-18 17:23:47 +0500

    18-11-2022 (1)

--- js/custom.js
+++ js/custom.js
@@ -1,109 +1,230 @@
-$(document).ready(function () {
-    $("#cnic").mask("99999-9999999-9");
-    // $("#account-no").mask("9999999999999999");
-    $("#vendor-cnic").mask("99999-9999999-9");
-    $("#mobile-no").mask("9999-9999999");
-    $("#card-no").mask("9999 9999 9999 9999");
-    $("#card-expiry").mask("99/99");
-    $("#cvv").mask("999");
+var cnic = document.querySelector('#cnic');
+cnic.addEventListener('keyup', function(e) {
+	if (event.key != 'Backspace' && (cnic.value.length === 5 || cnic.value.length === 13)) {
+  		cnic.value += '-';
+  	}
 });
 
-$(document).ready(function() {
-	$(document).on('click', '.finish', function(e) {
-		if($('#bank-confirmation').hasClass('d-none')) {
-			$('#bank-confirmation').removeClass('d-none');
-			$('#bank-confirmation').addClass('d-block');
-			$('#payment').addClass('d-none');
-		}
-	});
-
-	$(document).on('click', '.wallet-div', function(e) {
-		if($('#wallet-confirmation').hasClass('d-none')) {
-			$('#wallet-confirmation').removeClass('d-none');
-			$('#wallet-confirmation').addClass('d-block');
-			$('#payment').addClass('d-none');
-			$('#myModal').removeClass('show');
-			$('#myModal').addClass('d-none');
-			$('.modal-backdrop').removeClass('show');
-			$('.modal-backdrop').addClass('d-none');
-			$('.modal-open').css({"overflow": ""});
-		}
-	});
-
-	$(document).on('click', '.proceed', function(e) {
-		if($('#card-confirmation').hasClass('d-none')) {
-			$('#card-confirmation').removeClass('d-none');
-			$('#card-confirmation').addClass('d-block');
-			$('#payment').addClass('d-none');
-		}
-	});
-
-	$(document).on('click', '.home', function(e) {
-		if($('#bank-confirmation').hasClass('d-block')) {
-			$('#bank-confirmation').addClass('d-none');
-			$('#payment').removeClass('d-none');
-		}
-	});
-
-	$(document).on('click', '.home', function(e) {
-		if($('#wallet-confirmation').hasClass('d-block')) {
-			$('#wallet-confirmation').addClass('d-none');
-			$('#payment').removeClass('d-none');
-		}
-	});
-	
-	$(document).on('click', '.home', function(e) {
-		if($('#card-confirmation').hasClass('d-block')) {
-			$('#card-confirmation').addClass('d-none');
-			$('#payment').removeClass('d-none');
-		}
-	});
-
+var vcnic = document.querySelector('#vendor-cnic');
+vcnic.addEventListener('keyup', function(e) {
+	if (event.key != 'Backspace' && (vcnic.value.length === 5 || vcnic.value.length === 13)) {
+  		vcnic.value += '-';
+  	}
 });
 
-// Validate Form
+var mobile = document.querySelector('#mobile-no');
+mobile.addEventListener('keyup', function(e) {
+	if (event.key != 'Backspace' && (mobile.value.length === 4)) {
+  		mobile.value += '-';
+  	}
+});
 
-(function () {
-	'use strict'
-  
-	// Fetch all the forms we want to apply custom Bootstrap validation styles to
-	var forms = document.querySelectorAll('.needs-validation')
-  
-	// Loop over them and prevent submission
-	Array.prototype.slice.call(forms)
-	  .forEach(function (form) {
-		form.addEventListener('submit', function (event) {
-		  if (!form.checkValidity()) {
-			event.preventDefault()
-			event.stopPropagation()
-		  }
-  
-		  form.classList.add('was-validated')
-		}, false)
-	  })
-})()
+var card = document.querySelector('#card-no');
+card.addEventListener('keyup', function(e) {
+	if (event.key != 'Backspace' && (card.value.length === 4 || card.value.length === 9 || card.value.length === 14)) {
+  		card.value += ' ';
+  	}
+});
 
-// Session
+var cexpiry = document.querySelector('#card-expiry');
+cexpiry.addEventListener('keyup', function(e) {
+	if (event.key != 'Backspace' && (cexpiry.value.length === 2)) {
+  		cexpiry.value += '/';
+  	}
+});
 
-var deadline = new Date("Nov 16, 2022 00:00:00").getTime();
-var x = setInterval(function() {
-var now = new Date().getTime();
-var t = deadline - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("timer").innerHTML = "<div class='minutes'>" + minutes + "</div> : <div class='seconds'>" + seconds + '</div>';
-    if (t < 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "EXPIRED";
+$('#account-no').bind('keypress', function (event) {
+    var regex = new RegExp("^[a-zA-Z0-9\b]+$");
+    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+       event.preventDefault();
+       return false;
     }
-}, 1000);
+});
 
-function clickEvent(first,last){
-    if(first.value.length){
-        document.getElementById(last).focus();
-    }
+// Bank Form
+
+var currentTab = 0;
+document.addEventListener("DOMContentLoaded", function(event) {
+	showTab(currentTab);
+});
+
+function showTab(n) {
+	var x = document.getElementsByClassName("tab");
+	x[n].style.display = "block";
+	if(n == 0) {
+		document.getElementById("prevBtn").style.display = "none";
+		document.getElementById("pay").style.display = "none";
+	} else {
+		document.getElementById("prevBtn").style.display = "inline";
+		document.getElementById("pay").style.display = "none";
+	}
+	if(n == (x.length - 1)) {
+		document.getElementById("pay").style.display = "inline";
+		document.getElementById("nextBtn").style.display = "none";
+		//document.getElementById("nextBtn").innerHTML = "Submit <i class='fa fa-arrow-right'></i>";
+	} else {
+		document.getElementById("pay").style.display = "none";
+		document.getElementById("nextBtn").style.display = "inline";
+		//document.getElementById("nextBtn").innerHTML = "Next <i class='fa fa-arrow-right'></i>";
+	}
+	fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+	var x = document.getElementsByClassName("tab");
+	if(n == 1 && !validateForm()) return false;
+	x[currentTab].style.display = "none";
+	currentTab = currentTab + n;
+	if(currentTab >= x.length) {
+		// document.getElementById("regForm").submit();
+		// return false;
+		//alert("sdf");
+		document.getElementById("nextprevious").style.display = "none";
+		document.getElementById("bank-steps").style.display = "none";
+		document.getElementById("register").style.display = "none";
+		document.getElementById("payment").style.display = "none";
+		document.getElementById("bank-confirmation").style.display = "block";
+	}
+	showTab(currentTab);
+}
+
+function validateForm() {
+	var x, y, i, valid = true;
+	x = document.getElementsByClassName("tab");
+	y = x[currentTab].getElementsByTagName("input");
+	for(i = 0; i < y.length; i++) {
+		if(y[i].value == "") {
+			y[i].className += " invalid";
+			valid = false;
+		}
+	}
+
+	if(valid) {
+		document.getElementsByClassName("bank-step")[currentTab].className += " finish";
+	}
+	return valid;
+}
+
+function fixStepIndicator(n) {
+	var i, x = document.getElementsByClassName("bank-step");
+	for(i = 0; i < x.length; i++) {
+		x[i].className = x[i].className.replace(" active", "");
+	}
+	x[n].className += " active";
+}
+
+function formatBank (bank) {
+	if (!bank.id) {
+	  return bank.text;
+	}
+	var baseUrl = "images/bank";
+	var $bank = $(
+	  '<span><img src="' + baseUrl + '/' + bank.element.value.toLowerCase() + '.png" class="img-flag" /> ' + bank.text + '</span>'
+	);
+	return $bank;
+};
+
+$(".vendor").select2({
+	templateResult: formatWallet
+});
+
+// Wallet Bank
+
+const nextButton = document.getElementById("next");
+const backButton = document.getElementById("back");
+
+const pages = $("#assessment-form .page");
+const firstPage = $(pages).first()[0];
+const lastPage = $(pages).last()[0];
+const activeClass = "active";
+const nextButtonContent = "Proceed <i class='fa fa-arrow-right pt-1 ps-1'></i>";
+nextButton.addEventListener("click", function() {
+	const activePage = $("#assessment-form .page.active");
+	const isValid = validatePage(activePage);
+	if(isValid) {
+		nextPage(activePage);
+	} else {
+		var element = document.getElementById("mobile-no");
+		element.classList.add("valid");
+		var element = document.getElementById("vendor-cnic");
+		element.classList.add("valid");
+	}
+});
+
+backButton.addEventListener("click", function() {
+	const activePage = $("#assessment-form .page.active");
+	previousPage(activePage);
+});
+
+/**
+ * Navigate to the next form page.
+ * @param {HTMLElement} element The current page element
+ * @returns void on it being the last page.
+ */
+function nextPage(element) {
+	const onLastPage = $(lastPage).hasClass(activeClass);
+	if(onLastPage) {
+		submitForm();
+		return;
+	}
+	$(element).removeClass(activeClass);
+	const nextElement = $(element).next();
+	if(nextElement[0] === lastPage) {
+		nextButton.innerHTML = "Submit Assessment";
+		$('#next').removeClass("d-flex");
+	}
+	nextElement.addClass(activeClass);
+	if(!$(firstPage).hasClass(activeClass)) {
+		$(backButton).css("display", "unset");
+	}
+}
+
+/**
+ * Submitting the website assessment form to firebase real-time database.
+ */
+function submitForm() {
+	const form = $("#assessment-form");
+	const submittedValues = $(form).serializeArray();
+	const formValues = {};
+	for(submittedValue of submittedValues) {
+		formValues[submittedValue.name] = submittedValue.value;
+	}
+	$(lastPage).removeClass("active");
+	$(firstPage).addClass("active");
+	$('#myModal').modal('show');
+	// $('#myModal').removeClass("d-none");
+	// $('#myModal').addClass("d-block");
+}
+
+/**
+ * Navigate to the previus form page.
+ * @param {HTMLElement} element The active page element
+ */
+function previousPage(element) {
+	$(element).removeClass(activeClass);
+	const previousElement = $(element).prev();
+	if(previousElement[0] === firstPage) {
+		$(backButton).css("display", "none");
+	}
+	nextButton.innerHTML = nextButtonContent;
+	$(previousElement).addClass(activeClass);
+}
+
+/**
+ * Validates a form page.
+ * @param {HTMLElement} page The current page being validated
+ * @returns {Boolean} If the page is successully validated or not.
+ */
+function validatePage(page) {
+	const formElements = $(page).find("input, textarea, select");
+	for(formElement of formElements) {
+		const value = formElement.value;
+		if(!value) {
+			return false;
+		}
+	}
+	return true;
 }
 
 // OTP
@@ -171,117 +292,6 @@ function otptimer(remaining) {
 
 otptimer(120);
 
-// Milti Step Form
-
-const nextButton = document.getElementById("next");
-const backButton = document.getElementById("back");
-const payButton = document.getElementById("pay");
-
-const pages = $(".assessment-form .page");
-const firstPage = $(pages).first()[0];
-const lastPage = $(pages).last()[0];
-const activeClass = "active";
-const nextButtonContent = "Proceed <i class='fa fa-arrow-right'></i>";
-nextButton.addEventListener("click", function() {
-	const activePage = $(".assessment-form .page.active");
-	const isValid = validatePage(activePage);
-	if(isValid) {
-		nextPage(activePage);
-	} 
-	else {
-		document.getElementById("error").innerHTML = "Please fill all required fields";
-		//alert("There are inputs that are not valid. Please fill out all fields and check to make sure your email is valid.");
-	}
-});
-
-
-backButton.addEventListener("click", function() {
-	const activePage = $(".assessment-form .page.active");
-	previousPage(activePage);
-});
-
-/**
- * Navigate to the next form page.
- * @param {HTMLElement} element The current page element
- * @returns void on it being the last page.
- */
-
-function nextPage(element) {
-	const onLastPage = $(lastPage).hasClass(activeClass);
-	if(onLastPage) {
-		submitForm();
-		return;
-	}
-	$(element).removeClass(activeClass);
-	const nextElement = $(element).next();
-	if(nextElement[0] === lastPage) {
-		nextButton.innerHTML = "Proceed <i class='fa fa-arrow-right'></i>";
-	}
-	nextElement.addClass(activeClass);
-	if(!$(firstPage).hasClass(activeClass)) {
-		$(backButton).css("display", "unset");
-		$(backButton).css("margin-right", "5px");
-		$(nextButton).css("display", "none");
-		$(payButton).css("display", "unset");
-	}
-}
-
-function submitForm() {
-	const form = $(".assessment-form");
-	const submittedValues = $(form).serializeArray();
-	const formValues = {};
-	for(submittedValue of submittedValues) {
-		formValues[submittedValue.name] = submittedValue.value;
-	}
-	window.submitAssessment(formValues);
-	//alert("Thank you for submitting an inquiry. You will be contacted by email or your preferred contact method shortly.");
-	$(form)[0].reset();
-	$(lastPage).removeClass("active");
-	$(firstPage).addClass("active");
-}
-
-/**
- * Navigate to the previus form page.
- * @param {HTMLElement} element The active page element
- */
-function previousPage(element) {
-	$(element).removeClass(activeClass);
-	const previousElement = $(element).prev();
-	if(previousElement[0] === firstPage) {
-		$(backButton).css("display", "none");
-	}
-	nextButton.innerHTML = nextButtonContent;
-	$(previousElement).addClass(activeClass);
-}
-
-/**
- * Validates a form page.
- * @param {HTMLElement} page The current page being validated
- * @returns {Boolean} If the page is successully validated or not.
- */
-
-function validatePage(page) {
-	const formElements = $(page).find("input, select");
-	for(formElement of formElements) {
-		const value = formElement.value;
-		if(!value) {
-			return false;
-		}
-	}
-	return true;
-}
-
-function formatBank (bank) {
-	if (!bank.id) {
-	  return bank.text;
-	}
-	var baseUrl = "images/bank";
-	var $bank = $(
-	  '<span><img src="' + baseUrl + '/' + bank.element.value.toLowerCase() + '.png" class="img-flag" /> ' + bank.text + '</span>'
-	);
-	return $bank;
-};
-  
 $(".bank").select2({
 	templateResult: formatBank
 });
@@ -296,14 +306,56 @@ function formatWallet (wallet) {
 	);
 	return $wallet;
 };
-  
-$(".vendor").select2({
-	templateResult: formatWallet
-});
 
-$(document).ready(function(){
-	$('#form-3').on('submit', function(e){
-		$('#myModal').modal('show');
-		e.preventDefault();
-	})
+$(document).ready(function() {
+	$(document).on('click', '#pay', function(e) {
+		if($('#bank-confirmation').hasClass('d-none')) {
+			$('#bank-confirmation').removeClass('d-none');
+			$('#bank-confirmation').addClass('d-block');
+			$('#payment').addClass('d-none');
+		}
+	});
+
+	$(document).on('click', '.wallet-div', function(e) {
+		if($('#wallet-confirmation').hasClass('d-none')) {
+			$('#wallet-confirmation').removeClass('d-none');
+			$('#wallet-confirmation').addClass('d-block');
+			$('#payment').addClass('d-none');
+			$('#myModal').removeClass('show');
+			$('#myModal').addClass('hide');
+			$('.modal-backdrop').removeClass('show');
+			$('.modal-backdrop').addClass('hide');
+			$('.modal-open').css({"overflow": ""});
+		}
+	});
+
+	$(document).on('click', '.proceed', function(e) {
+		if($('#card-confirmation').hasClass('d-none')) {
+			$('#card-confirmation').removeClass('d-none');
+			$('#card-confirmation').addClass('d-block');
+			$('#payment').addClass('d-none');
+		}
+	});
+
+	$(document).on('click', '.home', function(e) {
+		if($('#bank-confirmation').hasClass('d-block')) {
+			$('#bank-confirmation').addClass('d-none');
+			$('#payment').removeClass('d-none');
+		}
+	});
+
+	$(document).on('click', '.home', function(e) {
+		if($('#wallet-confirmation').hasClass('d-block')) {
+			$('#wallet-confirmation').addClass('d-none');
+			$('#payment').removeClass('d-none');
+		}
+	});
+	
+	$(document).on('click', '.home', function(e) {
+		if($('#card-confirmation').hasClass('d-block')) {
+			$('#card-confirmation').addClass('d-none');
+			$('#payment').removeClass('d-none');
+		}
+	});
+
 });
